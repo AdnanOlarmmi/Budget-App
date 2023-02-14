e221235aa801e8419bdf3490bf1bc707b93cc853
AdnanOlarmmi <olarmmi@gmail.com>
2023-02-14 09:25:53 +0100

    Added devise views

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root "groups#index"
+
+  resources :groups, only: [:index, :show, :new, :create] do
+    resources :items, only: [:index, :create, :new, :show]
+  end
 end
