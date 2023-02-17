5dd7879b786d175ca5935c106001003eefc2f3fd
AdnanOlarmmi <olarmmi@gmail.com>
2023-02-17 23:30:27 +0100

    Add bootstrap to sign in and sign up pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root "home#index"
+  root "groups#index"
 
   resources :groups, only: [:index, :show, :new, :create] do
     resources :items, only: [:index, :create, :new, :show]
