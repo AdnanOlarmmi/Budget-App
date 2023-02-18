e8e97d9bc000dd5c76a510a3b18ca1cf0019dc3f
AdnanOlarmmi <olarmmi@gmail.com>
2023-02-18 10:32:12 +0100

    Finish home redirection for none authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,15 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   root "groups#index"
+  #resource for home index
+  resources :home, only: [:index]
 
   resources :groups, only: [:index, :show, :new, :create] do
     resources :items, only: [:index, :create, :new, :show]
